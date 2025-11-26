6f05aee33905b6286feaa388f171919b81612802
tariqul420 <tariqul@tariqul.dev>
2025-11-26 21:47:58 +0600

    Refactor server script to use ts-node-dev for development and add health check and user retrieval endpoints

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,27 @@ const server: Server = createServer(
         })
       );
     }
+
+    if (req.url === "/api" && req.method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Health status is okay.",
+          path: req.url,
+        })
+      );
+    }
+
+    if (req.url === "/api/users" && req.method === "POST") {
+      const user = {
+        id: 123,
+        name: "Tariqul Islam",
+        email: "tariqul@tariqul.dev",
+      };
+
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify(user));
+    }
   }
 );
 
